14a44c9e4771aa22961e2add9e867ca6b3eed0b3
vmarjunior <valdir.mjunior@hotmail.com>
2020-03-27 15:42:53 -0300

    implementação de testes unitários e integrados.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const { errors } = require("celebrate");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
 
 /**
  * Rota / Recurso
@@ -36,9 +38,7 @@ app.listen(3333);
  * NoSQL: MongoDB, CouchDB, etc
  */
 
- /**
-  * Driver: SELECT * FROM users
-  * Query Builder: table('users').select('*').where(...)
-  */
-
-
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where(...)
+ */
